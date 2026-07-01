4e1ce3af0c37d546f3a666e3bf7f708036a05c38
Raymond Alexander Palacios Alva <rpalaciosa89@ucvvirtual.edu.pe>
2026-07-01 04:26:37 -0500

    fix(inc2): footers en BenjiRescate, ColorFun, Aritmi, Benjiban

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker — Edgy Benji
 // Precachea archivos esenciales. Los MP3 se cachean bajo demanda.
 
-const CACHE_NAME = 'edgy-benji-v39';
+const CACHE_NAME = 'edgy-benji-v40';
 
 // Solo archivos pequeños/esenciales — NADA de MP3 aquí
 const PRECACHE = [
